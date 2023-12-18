e8020ef3b47476b2950bc35c01d19ff9e81f9afe
LGJ <bmw95885835@gmail.com>
2023-12-18 17:47:51 +0900

    약간의 url을 고쳤습니다.

--- BlackOlive/src/main/java/com/blackolive/app/controller/exhibition/ExhibitionController.java
+++ BlackOlive/src/main/java/com/blackolive/app/controller/exhibition/ExhibitionController.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ExhibitionController {
 
-	@GetMapping("/getExhibition")
+	@GetMapping("/store/getExhibition")
 	public String getExhibition() {
 		
 		return "exhibition.exhibition";
 	}//
+	
+	@GetMapping("/store/main/getExhibition")
+	public String getMainExhibition() {
+		
+		return null;
+	}
 	
 } // class
