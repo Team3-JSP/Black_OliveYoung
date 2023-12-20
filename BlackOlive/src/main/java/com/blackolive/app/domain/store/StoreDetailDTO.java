38983a890794e4cee107e69197bf8f758644727e
SangMun <zbqm1919@naver.com>
2023-12-20 17:21:24 +0900

    작업중

--- BlackOlive/src/main/java/com/blackolive/app/domain/store/StoreDetailDTO.java
+++ BlackOlive/src/main/java/com/blackolive/app/domain/store/StoreDetailDTO.java
@@ -1,14 +1,15 @@
 package com.blackolive.app.domain.store;
 
+import java.util.Calendar;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class StoreDetailDTO {
 	private String storeId;
 	private String districtId;
@@ -25,8 +26,35 @@ public class StoreDetailDTO {
 	private String saturday;
 	private String sunday;
 	private String holiday;
+	private String openYN = "N"; // 영업 여부
 	
 	private List<StoreCategoryDTO> storeCategory;
 	private List<OliveServiceDTO> oliveService;
 	private List<StoreThumbnailDTO> storeThumbnail;
+	
+	public StoreDetailDTO(String storeId, String weekday, String saturday, String sunday) {
+		this.storeId = storeId;
+		this.weekday = weekday;
+		this.saturday = saturday;
+		this.sunday = sunday;
+		
+		// 매장 오픈 여부
+		Calendar today = Calendar.getInstance();
+		int dayOfWeek = today.get(Calendar.DAY_OF_WEEK);
+		String currTime = today.get(Calendar.HOUR_OF_DAY) + ":" + today.get(Calendar.MINUTE);
+		String[] times = null;
+		
+		if(dayOfWeek == 1) {
+			times = sunday.split(" - ");
+		} else if(dayOfWeek == 2) {
+			times = sunday.split(" - ");
+		} else {
+			times = weekday.split(" - ");
+		}
+		
+		if(currTime.compareTo(times[0]) >= 0 && currTime.compareTo(times[1]) < 0) 
+			this.openYN = "Y";
+	}
+	
+	
 }
